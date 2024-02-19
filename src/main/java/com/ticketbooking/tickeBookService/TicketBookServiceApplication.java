84e0ee389ff786e63f6219eef4482b77653bf882
Aryan Chauhan <aryan.chauhan@pkware.com>
2024-02-19 13:19:33 +0530

    project set up with version and eureka

---
+++ src/main/java/com/ticketbooking/tickeBookService/TicketBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ticketbooking.tickeBookService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookServiceApplication.class, args);
+	}
+
+}
